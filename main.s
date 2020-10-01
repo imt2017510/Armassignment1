d5ff2a43d14402279267007563ab3ef439380255
imt2017510 <33414553+imt2017510@users.noreply.github.com>
2020-10-01 16:33:31 +0530

    Update main.s

--- main.s
+++ main.s
@@ -6,11 +6,11 @@ _main FUNCTION
 	  MOV r2,#0x01;
 	  MOV r4,#0x01;
 	  CMP r2,r7;
-	  ITTEE LE
-	  SUBLE r2,r2,r7;
-	  MOVLE r0,#0x0;
-	  ADDGT r2,r2,r4;
-	  MOVGT r4,#0x0;
+	  ITTEE NE
+	  SUBNE r2,r2,r7;
+	  MOVNE r2,#0x0;
+	  ADDEQ r2,r2,r4;
+	  MOVEQ r4,#0x0;
    
    ENDFUNC
    END
